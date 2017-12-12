ebfeea8d9b655abb2b3343082329340984f18d61
thiswhy <1414770848@qq.com>
2017-12-12 22:31:31 +0800

    push all

---
+++ GFLottery/ios-packager.sh
@@ -0,0 +1,1 @@
+react-native bundle --platform ios --entry-file index.ios.js --bundle-output 'release/main.jsbundle' --assets-dest 'release' --dev false
---
+++ GFLottery/ios/GFLottery/android-packager.sh
@@ -0,0 +1,1 @@
+react-native bundle --platform android --entry-file index.android.js --bundle-output 'release/main.jsbundle' --assets-dest 'release' --dev false
